4a63c1b68d3657f9b71c5755980449c9fa469804
itgsod-pontus-wistrom <pontus.wistrom@elev.it-gymnasiet.se>
2016-03-16 10:52:32 +0100

    Uppgifter

--- uppgifter.rb
+++ uppgifter.rb
@@ -1,11 +1,36 @@
+#Uppgift 1
 a = [1,2,3]
 b = [4,5,6]
 c= []
 
-i=0
-while i < 3
-  c << a[i].to_s + b[i].to_s
+i = 0
+while i < a.length
+  c << a[i]*10 + b[i]
   i += 1
 end
 
-puts c
+p c
+
+#Uppgift 2
+
+a = [1,2]
+
+def switch(a,b)
+  arr = []
+  arr[0] = b
+  arr[1] = a
+  return arr
+end
+
+p switch(a[0],a[1])
+
+
+#Uppgift 3
+
+a = [1,2,3,4]
+
+def rotate()
+
+end
+
+p rotate()
